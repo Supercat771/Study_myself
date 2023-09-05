b338142315bfa109fa0b9e1b2d70689ce364d9e9
Supercat771 <122520268+Supercat771@users.noreply.github.com>
2023-09-06 03:22:44 +0900

    Add files via upload

---
+++ Study_Backjoon_stuydyGroup/src/myalgorithm/ForExample_1152_2.java
@@ -0,0 +1,41 @@
+package myalgorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class ForExample_1152_2 {
+
+	public static void main(String[] args) throws IOException {
+
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine().trim();
+		
+		if(str.equals("")) {
+			
+			System.out.println(0);
+			
+		}
+		
+		else {
+			
+			String[] strArr = str.split(" ");
+			
+			
+			
+			
+			
+			System.out.println(strArr.length);
+			
+		}
+		
+		
+		
+		
+		
+	
+		
+	}
+
+}
